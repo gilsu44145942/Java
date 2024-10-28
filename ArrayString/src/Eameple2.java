14ee1cdf43f65ce0cc4f42243acb719d9a7d0da8
gilsu44145942 <wjdrlftn6471@naver.com>
2024-10-28 17:11:32 +0900

    10/28

---
+++ ArrayString/src/Eameple2.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Eameple2 {
+    public static void main(String[] args) {
+        // 주어진 베열의 평균값을 계산하는 매서드
+        int[] numbers = {10,20,30,40,50};
+        System.out.println(calculateAverage(numbers)); //30
+    }
+    public static double calculateAverage(int[]arr ) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++){
+            sum = sum +arr[i]; // 배열의 데이터를 더함(arr[i])
+        }
+       double average = (double) sum / arr.length;
+        return average;
+    }
+}
---
+++ ArrayString/src/Eameple3.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+
+public class Eameple3 {
+    public static void main(String[] args) {
+        // 중복된 숫자의 객수를 리턴하는 매서드
+        int[] numbers = {1, 3, 7, 2, 4, 3, 9, 8, 3, 4};
+        System.out.println(CountNumber(numbers, 3)); //3
+        System.out.println(CountNumber(numbers, 4)); //2
+        System.out.println(CountNumber(numbers, 10)); //-1
+    }
+
+    public static int CountNumber(int[] numbers, int target) {
+        int count = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == target) {
+                count = count + 1;
+            }
+        }
+            if (count > 0) {
+                return count;
+            } else {
+                return -1;
+            }
+
+
+        }
+    }
+
+
+
